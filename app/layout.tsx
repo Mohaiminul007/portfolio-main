da01610b9412d318031d412d3a096231801c95a1
Mohaiminul007 <mohaimin.durjoy@gmail.com>
2024-02-24 18:21:55 +0600

    first comments

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+// import"../font/FiraCode-VariableFont_wght.ttf"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className='font-FiraCode'>{children}</body>
     </html>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,32 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors:{
+      gray:'#01080E',
+      teal:'#3C9D93',
+      white:'#FFFFFF',
+      orange:'#FEA55F',
+      emerald:'#43D9AD',
+      rose:'#E99287',
+      purple:'#C98BDF',
+      'slate-500':'#607B96',
+      'slate-800':'#1E2D3D',
+      'slate-900':'#011627',
+      'slate-950':'#011221',
+      'indigo-600':'#4D5BCE'
+
+    },
+    fontFamily:{
+      FiraCode :'Fira Code , monospace' 
+    },
+  fontSize: {
+    xs:'14px',
+    sm:'16px',
+    lg:'18px',
+    xl:'32px',
+    '2xl':'62px'
+
+  },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
